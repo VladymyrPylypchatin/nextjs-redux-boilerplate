2a658f8d4facbbb916e80575bc13270285f127fb
Cary Wolff <boss@caryyon.com>
2022-01-03 19:39:15 -0600

    spelling mistake

--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -3,7 +3,7 @@ import * as actionTypes from './actionTypes';
 export const counterIncriment = () => ({ type: actionTypes.counterIncriment });
 export const counterDecriment = () => ({ type: actionTypes.counterDecriment });
 
-//Example of the async actoins with redux-tunk
+//Example of the async actoins with redux-thunk
 export const counterIncrimentAsync = () => (dispatch) => {
     setTimeout(() => {
         dispatch(counterIncriment);
